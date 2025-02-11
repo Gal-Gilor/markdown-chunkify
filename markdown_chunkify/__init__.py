4084e0b80909223d20982ebe891ec5a81e58c8a1
Gal Gilor <gilorgal2010@gmail.com>
2025-02-10 20:28:30 -0500

    Added a pymupdf parser to utils.

--- markdown_chunkify/__init__.py
+++ markdown_chunkify/__init__.py
@@ -1,3 +1,4 @@
+from markdown_chunkify.utils.parsers import PyMuPDFMParser
 from markdown_chunkify.utils.splitters import MarkdownSplitter
 
-__all__ = ["MarkdownSplitter"]
+__all__ = ["MarkdownSplitter", "PyMuPDFMParser"]
